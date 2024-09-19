e9765077ee0c03b43467f05f8aa18fe4cc890647
jeeva269 <jeeva18623@gmail.com>
2024-09-19 15:28:49 +0530

    finally_project_completed

--- MyProject/WeatherApp/form.py
+++ MyProject/WeatherApp/form.py
@@ -1,8 +1,11 @@
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm,TextInput
 from .models import City
 
 class CityForm(ModelForm):
     class Meta:
-        model = City
-        fields = ["name"]
-        Widgets = {'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'City Name'})}
+        model=City
+        fields=["name"]
+        widgets={'name':TextInput(attrs={'class':'form-control','placeholder':'City Name'})}        
+
+        
+        
